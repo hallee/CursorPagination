d6b3233a715152b1f11e671f39a8a79c732a5c11
Hal Lee <hal@lee.me>
2019-02-13 19:45:27 -0500

    Switch to hallee/RuntimeExtensions

--- Package.swift
+++ Package.swift
@@ -9,14 +9,13 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-		.package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from:"3.0.0-rc.3.0.1")),
+		.package(url: "https://github.com/vapor/fluent.git", from:"3.0.0"),
 		.package(url: "https://github.com/Appsaurus/FluentTestApp", .upToNextMajor(from: "0.1.0")),
 		.package(url: "https://github.com/Appsaurus/CodableExtensions", .upToNextMajor(from: "1.0.0")),
-		.package(url: "https://github.com/Appsaurus/RuntimeExtensions", .upToNextMajor(from: "0.1.0"))
-
+		.package(url: "https://github.com/hallee/RuntimeExtensions", from: "0.1.3")
 	],
 	targets: [
 		.target(name: "CursorPagination", dependencies: ["Vapor", "Fluent", "CodableExtensions", "RuntimeExtensions"]),
-        .testTarget(name: "CursorPaginationTests", dependencies: ["CursorPagination", "FluentTestApp"]),
+		.testTarget(name: "CursorPaginationTests", dependencies: ["CursorPagination", "FluentTestApp"])
 	]
 )
